8a5cdb48d344c44107ec1d6ae14f447e93d6bf47
Yusup Maulana <yusupmaulana950@gmail.com>
2026-05-26 08:29:04 +0700

    fix(trpc): move TRPCReactProvider under html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,13 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <TRPCReactProvider>
-      <html
-        lang="en"
-        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      >
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <TRPCReactProvider>
         <body className="min-h-full flex flex-col">{children}</body>
-      </html>
-    </TRPCReactProvider>
+      </TRPCReactProvider>
+    </html>
   );
 }
